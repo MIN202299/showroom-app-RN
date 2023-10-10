61270060c11efb87487326afb5419bac5b0d20f0
MIN202299 <min202299@gmail.com>
2023-10-10 18:41:35 +0800

    chore: use react native blur view

--- screens/SelectScreen.js
+++ screens/SelectScreen.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import * as ScreenOrientation from 'expo-screen-orientation'
-
-import { useNavigation } from '@react-navigation/native'
 import { useEffect } from 'react'
+
+// 导航
+import { useNavigation } from '@react-navigation/native'
+
+// 背景模糊组件
+import { BlurView } from '@react-native-community/blur'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     fontFamily: 'PuHui',
+    color: 'white',
+
   },
   textBold: {
     fontFamily: 'PuHui-Bold',
@@ -24,7 +29,7 @@ export default function SelectScreen(props) {
   const navigation = useNavigation()
 
   useEffect(() => {
-    navigation.navigate('Vertical')
+    // navigation.navigate('Vertical')
     const unsubscribe = navigation.addListener('focus', () => {
       // 屏幕自动旋转
       ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
@@ -34,9 +39,12 @@ export default function SelectScreen(props) {
   }, [])
 
   return (
-    <View style={styles.container} onLayout={props.onLayoutRootView}>
-      <Text style={styles.textBold} onPress={() => navigation.navigate('Vertical')}>Select Screen</Text>
-      <StatusBar style='auto' hidden></StatusBar>
-    </View>
+    <ImageBackground source={require('../assets/bg/horizon-bg.png')} resizeMode='cover' style={styles.container}>
+      <View style={styles.container} onLayout={props.onLayoutRootView}>
+        <Text style={styles.text} onPress={() => navigation.navigate('Vertical')}>Select Screen</Text>
+        <BlurView style={{}}></BlurView>
+        <StatusBar style='auto' hidden></StatusBar>
+      </View>
+    </ImageBackground>
   )
 }
