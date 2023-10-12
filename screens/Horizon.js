f89f5303034249e66a0d02ed130b90ab0e14d85d
MIN202299 <min202299@gmail.com>
2023-10-12 14:01:11 +0800

    feat: 竖屏UI完成

--- screens/Horizon.js
+++ screens/Horizon.js
@@ -3,12 +3,17 @@ import { BackHandler, Image, ImageBackground, StyleSheet, Text, View } from 'rea
 import * as ScreenOrientation from 'expo-screen-orientation'
 
 import { useNavigation } from '@react-navigation/native'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { BlurView } from 'expo-blur'
 
 // video 组件
 import { ResizeMode, Video } from 'expo-av'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { companyData as companies } from '../data/companies'
+import { STORAGE_KEY } from '../store/constant'
+import { resourceMapping } from '../data/resourceMapping'
 
 const styles = StyleSheet.create({
   container: {
@@ -22,18 +27,25 @@ const styles = StyleSheet.create({
   },
 })
 
-export default function Horizon(props) {
+export default function Horizon() {
   const navigation = useNavigation()
   const video = useRef()
+  const [com, setCom] = useState()
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       // 屏幕锁定横向
       ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE)
     })
+    AsyncStorage.getItem(STORAGE_KEY).then((res) => {
+      if (res) {
+        const screenName = JSON.parse(res).screenName
+        const target = companies.find(item => item.screenName === screenName)
+        target && setCom(target)
+        // console.log(resourceMapping[target.product[0].src])
+      }
+    })
     return unsubscribe
   }, [])
-
-  const testVideoUri = '../assets/companies/kuangwei/video.mp4'
 
   // 返回键监听
   useEffect(() => {
@@ -46,6 +58,8 @@ export default function Horizon(props) {
   }, [])
 
   function handleAutoplay() {
+    if (!video)
+      return
     video.current.setPositionAsync(0)
     video.current.playAsync()
   }
@@ -54,26 +68,30 @@ export default function Horizon(props) {
     <ImageBackground source={require('../assets/bg/horizon-bg.png')} resizeMode='cover' style={styles.container}>
       <View style={{ width: '90%', height: '85%', padding: 2 }}>
         <BlurView intensity={20} style={{ flex: 1 }}>
-          <View style={{ flex: 1, padding: '5%' }}>
-            <View style={{ alignSelf: 'flex-start', paddingBottom: 3, marginLeft: 120, marginBottom: 4, borderBottomWidth: 4, borderStyle: 'dotted', borderBottomColor: '#fff' }}>
-              <Text style={[styles.textBold, { fontSize: 20, textShadowRadius: 4, textShadowColor: '#fff' }]}>某某公司</Text>
-            </View>
-            <ImageBackground source={require('../assets/bg/h-border.png')} resizeMode='contain' style={{ flex: 1 }}>
-              <View style={{ flex: 1, paddingVertical: '5%', paddingHorizontal: '15%' }}>
-                {/* media video */}
-                <View style={{ flex: 1 }}>
-                  <Video
-                    ref={video}
-                    source={require(testVideoUri)}
-                    resizeMode={ResizeMode.CONTAIN}
-                    useNativeControls
-                    isLooping
-                    onLoad={() => { handleAutoplay() }}
-                    style={{ flex: 1 }}></Video>
+          {
+            com && (
+              <View style={{ flex: 1, padding: '5%' }}>
+                <View style={{ alignSelf: 'flex-start', paddingBottom: 3, marginLeft: 120, marginBottom: 4, borderBottomWidth: 4, borderStyle: 'dotted', borderBottomColor: '#fff' }}>
+                  <Text style={[styles.textBold, { fontSize: 20, textShadowRadius: 4, textShadowColor: '#fff' }]}>{com.company}</Text>
                 </View>
+                <ImageBackground source={require('../assets/bg/h-border.png')} resizeMode='contain' style={{ flex: 1 }}>
+                  <View style={{ flex: 1, paddingVertical: '5%', paddingHorizontal: '15%' }}>
+                    {/* media video */}
+                    <View style={{ flex: 1 }}>
+                      <Video
+                        ref={video}
+                        source={resourceMapping[com.src]}
+                        resizeMode={ResizeMode.CONTAIN}
+                        useNativeControls
+                        isLooping
+                        onLoad={() => { handleAutoplay() }}
+                        style={{ flex: 1 }}></Video>
+                    </View>
+                  </View>
+                </ImageBackground>
               </View>
-            </ImageBackground>
-          </View>
+            )
+          }
         </BlurView>
         {/* 四个角 */}
         <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, left: 0, bottom: 0 }}></Image>
--- screens/Vertical.js
+++ screens/Vertical.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar'
-import { BackHandler, Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { BackHandler, Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native'
 import * as ScreenOrientation from 'expo-screen-orientation'
 import { BlurView } from 'expo-blur'
 
 import { useNavigation } from '@react-navigation/native'
 
-import { useEffect } from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+
+import { useEffect, useRef, useState } from 'react'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import PagerView from 'react-native-pager-view'
+import { ResizeMode, Video } from 'expo-av'
+import { STORAGE_KEY } from '../store/constant'
+import { companyData as companies } from '../data/companies'
+import { resourceMapping } from '../data/resourceMapping'
 
 const styles = StyleSheet.create({
   container: {
@@ -19,17 +28,25 @@ const styles = StyleSheet.create({
   },
 })
 
-export default function Vertical(props) {
+export default function Vertical() {
   const navigation = useNavigation()
+  const [com, setCom] = useState()
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       // 屏幕锁定横向
       ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
     })
+    AsyncStorage.getItem(STORAGE_KEY).then((res) => {
+      if (res) {
+        const screenName = JSON.parse(res).screenName
+        const target = companies.find(item => item.screenName === screenName)
+        target && setCom(target)
+        // console.log(resourceMapping[target.product[0].src])
+      }
+    })
     return unsubscribe
   }, [])
 
-  const testImageUri = '../assets/companies/beiming/s-1.gif'
   // 返回键监听
   useEffect(() => {
     const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
@@ -39,49 +56,112 @@ export default function Vertical(props) {
     })
     return () => backHandler.remove()
   }, [])
+  const video = useRef()
+  function handleAutoplay() {
+    if (!video)
+      return
+    video.current.setPositionAsync(0)
+    video.current.playAsync()
+  }
 
   return (
     <ImageBackground source={require('../assets/bg/vertical-bg.png')} resizeMode='cover' style={styles.container}>
-      <View style={{ width: '90%', height: '85%', padding: 2 }}>
-        <BlurView intensity={20} style={{ flex: 1 }}>
-          <View style={{ flex: 1, paddingVertical: '10%', paddingHorizontal: '2%' }}>
-            <View style={{ flex: 1 }}>
-              <View style={{ alignSelf: 'flex-start', paddingBottom: 3, marginLeft: 82, marginBottom: 4, borderBottomWidth: 4, borderStyle: 'dotted', borderBottomColor: '#fff' }}>
-                <Text style={[styles.textBold, { fontSize: 20, textShadowRadius: 4, textShadowColor: '#fff' }]}>某某公司</Text>
-              </View>
-              <ImageBackground source={require('../assets/bg/v-border.png')} resizeMode='contain' style={{ flex: 1, paddingVertical: '6%', paddingLeft: '13%', paddingRight: '8%' }}>
+      {
+        com && (
+          <View style={{ width: '90%', height: '85%', padding: 2 }}>
+            <BlurView intensity={20} style={{ flex: 1 }}>
+              <View style={{ flex: 1, paddingVertical: '10%', paddingHorizontal: '2%' }}>
                 <View style={{ flex: 1 }}>
-                  <View style={{ marginLeft: 14 }}>
-                    <Text style={[styles.textBold, { fontSize: 16 }]}>产品标题</Text>
+                  <View style={{ alignSelf: 'flex-start', paddingBottom: 3, marginLeft: 82, marginBottom: 4, borderBottomWidth: 4, borderStyle: 'dotted', borderBottomColor: '#fff' }}>
+                    <Text style={[styles.textBold, { fontSize: 20, textShadowRadius: 4, textShadowColor: '#fff' }]}>{com.company}</Text>
                   </View>
-                  <View style={{ flexWrap: 'nowrap', flexDirection: 'row', gap: 10, marginTop: 4 }}>
-                    <View style={{ flex: 1, aspectRatio: 1, backgroundColor: 'red', padding: 1, borderRadius: 5, overflow: 'hidden' }}>
-                      <View style={{ flex: 1, borderRadius: 4, overflow: 'hidden' }}>
-                        <Image source={require(testImageUri)} resizeMode='cover' style={{ width: '100%', height: '100%' }}></Image>
+                  <ImageBackground source={require('../assets/bg/v-border.png')} resizeMode='contain' style={{ flex: 1, paddingVertical: '6%', paddingLeft: '13%', paddingRight: '8%' }}>
+                    <View style={{ flex: 1, paddingBottom: 10 }}>
+                      {/* 产品标题 */}
+                      <View style={{ marginLeft: 14 }}>
+                        <Text style={[styles.textBold, { fontSize: 16 }]}>{com.productName}</Text>
+                      </View>
+                      {/* 产品介绍 */}
+                      <View style={{ flexWrap: 'nowrap', flexDirection: 'row', gap: 10, marginTop: 4 }}>
+                        <LinearGradient colors={['#D8D8D8aa', 'transparent', '#D8D8D8aa']} style={{ flex: 1, aspectRatio: 1, padding: 1, borderRadius: 5, overflow: 'hidden' }}>
+                          <View style={{ flex: 1, borderRadius: 4, overflow: 'hidden' }}>
+                            <Image source={resourceMapping[com.product[0].src]} resizeMode='cover' style={{ width: '100%', height: '100%' }}></Image>
+                          </View >
+                        </LinearGradient>
+                        <LinearGradient colors={['#D8D8D8aa', 'transparent', '#D8D8D8aa']} style={{ flex: 1, aspectRatio: 1, padding: 1, borderRadius: 5, overflow: 'hidden' }}>
+                          <View style={{ flex: 1, borderRadius: 4, overflow: 'hidden' }}>
+                            <Image source={resourceMapping[com.product[1].src]} resizeMode='cover' style={{ width: '100%', height: '100%' }}></Image>
+                          </View>
+                        </LinearGradient>
+                        <View style={{ flex: 1, aspectRatio: 1, padding: 1, borderRadius: 5, overflow: 'hidden' }}>
+                          {/* 产品标题 */}
+                          <View style={{ marginBottom: 5 }}>
+                            <Text style={[styles.textBold, { fontSize: 10 }]}>{com.productName}</Text>
+                          </View>
+                          <ScrollView style={{ flex: 1, overflow: 'scroll' }}>
+                            <Text style={{ fontSize: 10, textAlign: 'justify', color: '#fff' }}>{com.productIntro}</Text>
+                          </ScrollView>
+                        </View>
+                      </View>
+                      {/* 产品介绍 */}
+                      <View style={{ flex: 1, marginVertical: 10, borderWidth: 1, borderColor: '#D8D8D8aa', padding: 10, borderRadius: 5 }}>
+                        { // swiper
+                          com.mediaType === 'video'
+                            ? (
+                              <Video
+                                ref={video}
+                                source={resourceMapping[com.src]}
+                                resizeMode={ResizeMode.CONTAIN}
+                                style={{ flex: 1 }}
+                                useNativeControls
+                                isLooping
+                                onLoad={() => { handleAutoplay() }}></Video>
+                            )
+                            : (
+                              com.mediaType === 'swipe-image'
+                                ? (<PagerView style={{ flex: 1 }} initialPage={0}>
+                                  {
+                                    Array.isArray(com.src)
+                                      ? com.src.map((src, idx) => (
+                                        <View style={{
+                                          flex: 1,
+                                          justifyContent: 'center',
+                                          alignItems: 'center',
+                                        }} key={idx}>
+                                          <Image source={resourceMapping[src]} style={{ width: '100%', height: '100%' }} resizeMode='contain'></Image>
+                                        </View>
+                                      ))
+                                      : (<View>
+                                        <Image source={resourceMapping[com.src]} style={{ flex: 1 }} resizeMode='contain'></Image>
+                                      </View>)
+                                  }
+                                </PagerView>)
+                                : null
+                            )
+                        }
+                      </View>
+                      {/* 公司介绍 */}
+                      <View style={{ height: 200, borderWidth: 1, borderColor: '#D8D8D8aa', padding: 10, borderRadius: 5 }}>
+                        <View style={{ marginBottom: 4 }}>
+                          <Text style={[styles.textBold, { fontSize: 16 }]}>{com.company}</Text>
+                        </View>
+                        <ScrollView style={{ flex: 1 }}>
+                          <Text style={[{ fontSize: 12, color: '#fff' }]}>{com.companyIntro}</Text>
+                        </ScrollView>
                       </View>
                     </View>
-                    <View style={{ flex: 1, aspectRatio: 1, backgroundColor: 'red', padding: 1, borderRadius: 5, overflow: 'hidden' }}>
-                      <View style={{ flex: 1, borderRadius: 4, overflow: 'hidden' }}>
-                        <Image source={require(testImageUri)} resizeMode='cover' style={{ width: '100%', height: '100%' }}></Image>
-                      </View>
-                    </View>
-                    <View style={{ flex: 1, aspectRatio: 1, backgroundColor: 'red', padding: 1, borderRadius: 5, overflow: 'hidden' }}>
-                      <View style={{ flex: 1, borderRadius: 4, overflow: 'hidden' }}>
-                        <Image source={require(testImageUri)} resizeMode='cover' style={{ width: '100%', height: '100%' }}></Image>
-                      </View>
-                    </View>
-                  </View>
+                  </ImageBackground>
                 </View>
-              </ImageBackground>
-            </View>
+              </View>
+            </BlurView>
+            {/* 四个角 */}
+            <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, left: 0, bottom: 0 }}></Image>
+            <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, left: 0, top: 0, transform: [{ rotate: '90deg' }] }}></Image>
+            <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, right: 0, bottom: 0, transform: [{ rotate: '-90deg' }] }}></Image>
+            <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, right: 0, top: 0, transform: [{ rotate: '-180deg' }] }}></Image>
           </View>
-        </BlurView>
-        {/* 四个角 */}
-        <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, left: 0, bottom: 0 }}></Image>
-        <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, left: 0, top: 0, transform: [{ rotate: '90deg' }] }}></Image>
-        <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, right: 0, bottom: 0, transform: [{ rotate: '-90deg' }] }}></Image>
-        <Image source={require('../assets/bg/corner.png')} style={{ position: 'absolute', width: 100, height: 100, right: 0, top: 0, transform: [{ rotate: '-180deg' }] }}></Image>
-      </View>
+        )
+      }
       <StatusBar style='auto' hidden></StatusBar>
     </ImageBackground>
   )
