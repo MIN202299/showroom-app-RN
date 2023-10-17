9912c78714b0f0de8e19704c818847b40732709f
MIN202299 <min202299@gmail.com>
2023-10-17 14:27:17 +0800

    chore:update

--- store/constant.js
+++ store/constant.js
@@ -1,6 +1,6 @@
 export const STORAGE_KEY = 'showroom-storage'
 
-export const SOCKET_URL = 'http://192.168.2.119:8422'
+export const SOCKET_URL = 'http://192.168.1.69:8422'
 
 export const Theme = {
   DEFAULT: '0',
